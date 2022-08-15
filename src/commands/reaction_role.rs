396c67710bcdbcff7bd6aca5bdad2210d9cfb816
Dra <bakugo.13@gmail.com>
2022-08-15 16:43:15 +0200

    Adding a working reaction handling and working DB
    Still lacking the insert part coming through the commit containing the slash command

--- src/commands/reaction_role.rs
+++ src/commands/reaction_role.rs
@@ -1,13 +1,96 @@
 use poise::{FrameworkContext, Event, Event::ReactionAdd};
+use poise::serenity_prelude::{Context, Emoji, Reaction, ReactionType::{Custom, Unicode}, RoleId};
 use super::super::helper::*;
+use sea_orm::{
+    ActiveValue::Set, ColumnTrait, Condition, DatabaseConnection,
+    QueryFilter, EntityTrait, ActiveModelTrait
+};
+
+use core::default::Default as BaseDefault;
+use poise::futures_util::SinkExt;
+use crate::entity::{rdc_reactionrole, prelude::RdcReactionrole};
+
+
+// fetch one item
+// let rr: Option<entity::rdc_reactionrole::Model> = RdcReactionrole::find_by_id(1).one(&db).await.expect("Grr");
+// if let None = rr {
+//     println!("Rn, that's okay")
+// }
+
+// insert one item
+async fn insert_role_in_db(guild_id:u64, channel_id:u64, message_id:u64, role_id:u64, emoji: String) {
+    // let emoji_as_str:str;
+    //
+    // if let Custom { id } = emoji {
+    //
+    // }
+
+    // let reaction_role = rdc_reactionrole::ActiveModel {
+    //     guild_id: Set(guild_id as i64),
+    //     channel_id: Set(channel_id as i64),
+    //     message_id: Set(message_id as i64),
+    //     role_id: Set(role_id as i64),
+    //     emoji: Set("❤️".to_owned()),
+    //     ..BaseDefault::default()
+    // };
+    //
+    // let reaction_role: rdc_reactionrole::Model = reaction_role.insert(&db).await.expect("Duh you can't insert here");
+
+}
+
 
 /// Displays your or another user's account creation date
 pub async fn rr(
+    context: &Context,
     event: &Event<'_>,
-    _framework_ctx: FrameworkContext<'_, Data, Error >)
+    framework: FrameworkContext<'_, Data, Error >)
     -> Result<(),Error>{
+    let db = &framework.user_data.db;
+
     if let ReactionAdd { add_reaction} = event {
-        println!("{:?}",add_reaction)
+        let user_id = add_reaction.user_id.unwrap().0;
+        let gid = match add_reaction.guild_id {
+            Some(gid) => gid.0,
+            _ => 0
+        };
+        let message_id = add_reaction.message_id.0;
+        let channel_id = add_reaction.channel_id.0;
+        let emoji = match &add_reaction.emoji {
+            Custom { id, .. } => id.to_string(),
+            Unicode(emote) => emote.to_string(),
+            _ => panic!("Did not expect to get something else than Custom or Unicode !")
+        };
+
+        let conditions = Condition::all()
+            .add(rdc_reactionrole::Column::GuildId.eq(gid))
+            .add(rdc_reactionrole::Column::MessageId.eq(message_id))
+            .add(rdc_reactionrole::Column::ChannelId.eq(channel_id))
+            .add(rdc_reactionrole::Column::Emoji.eq(emoji));
+
+        let rr: Option<rdc_reactionrole::Model> = RdcReactionrole::find()
+            .filter(conditions)
+            .one(db).await.expect("Grr");
+
+        let http = &context.http;
+        match rr {
+            Some(reac) => {
+                let member = &add_reaction.member;
+                let member = member.as_ref().unwrap();
+                let role_id = reac.role_id as u64;
+                println!("User {} in {} asked for {}", user_id, gid, role_id);
+                if member.roles.contains(&RoleId(role_id)){
+                    println!("--- Removing role {} ---", role_id);
+                    http.remove_member_role(gid, user_id, role_id,None).await?;
+                } else {
+                    println!("--- Giving role {} ---", role_id);
+                    http.add_member_role(gid, user_id, role_id, None).await?;
+                }
+                println!("Deleting user reaction");
+                http.delete_reaction(channel_id, message_id, Some(user_id), &add_reaction.emoji).await?;
+
+            },
+            None => println!("No role to give !")
+        }
     }
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
 mod commands;
+mod entity;
 
+use std::time::Duration;
 use poise::{serenity_prelude as serenity};
-use crate::commands::*;
+use sea_orm::{ConnectOptions, Database, DatabaseConnection};
+use crate::commands::reaction_role;
+
 
 pub mod helper {
+    use sea_orm::DatabaseConnection;
+
     pub type Error = Box<dyn std::error::Error + Send + Sync>;
     pub type Context<'a> = poise::Context<'a, Data, Error>;
     // User data, which is stored and accessible in all command invocations
-    pub struct Data {}
+    pub struct Data {
+        pub(crate) db: DatabaseConnection
+    }
 }
 
 use crate::helper::*;
+
+
 
 /// Displays your or another user's account creation date
 #[poise::command(slash_command)]
@@ -29,21 +39,41 @@ pub async fn register(ctx: Context<'_>, #[flag] global: bool) -> Result<(), Erro
     poise::builtins::register_application_commands(ctx, global).await.map_err(Into::into)
 }
 
+async fn create_db_connection() -> Result<DatabaseConnection, sea_orm::DbErr> {
+    let db_connector = std::env::var("DB_CONNECTOR")
+        .expect("missing db connector; format : protocol://username:password@localhost/database");
+
+    let mut opt = ConnectOptions::new(db_connector.to_owned());
+    opt.max_connections(10)
+        .min_connections(5)
+        .connect_timeout(Duration::from_secs(8))
+        .idle_timeout(Duration::from_secs(8))
+        .max_lifetime(Duration::from_secs(8));
+
+    Database::connect(opt).await
+}
+
 #[tokio::main]
 async fn main() {
     let token = std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN");
 
+    let db = create_db_connection().await.expect("Couldn't create a DatabaseConnection.");
+
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
             commands: vec![age(), register()],
-            listener: |_, event, framework, _| {
-                Box::pin(reaction_role::rr(/*Context::from(ctx), */event, framework))
+            listener: |ctx, event, framework, _| {
+                Box::pin(reaction_role::rr(ctx, event, framework))
             },
             ..Default::default()
         })
         .token(token) // Memo for fish : `set -gx DISCORD_TOKEN`
         .intents(serenity::GatewayIntents::non_privileged())
-        .user_data_setup(move |_ctx, _ready, _framework| Box::pin(async move { Ok(Data {}) }));
+        .user_data_setup(move |_ctx, _ready, _framework| {
+            Box::pin(async move {
+                Ok(Data { db })
+            })
+        });
 
 
     println!("Built the framework");
